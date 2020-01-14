5d6f9873fa4e3c05aef3a38e18f418ad582ecde6
Heather Shores <Heather@Heathers-MacBook-Air.local>
2020-01-14 15:24:40 -0600

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor amet skateboard glossier gastropub, fanny pack tumeric
+        swag chambray synth. Single-origin coffee gastropub deep v, jianbing
+        cold-pressed small batch letterpress sartorial. Farm-to-table
+        williamsburg intelligentsia, iceland taxidermy salvia air plant. Narwhal
+        vice kinfolk hashtag photo booth, semiotics health goth YOLO. Hoodie
+        kogi palo santo jean shorts, direct trade DIY taxidermy XOXO vice
+        leggings fam. Hashtag put a bird on it poutine four dollar toast,
+        fashion axe kitsch cloud bread.
+      </p>
+    </section>
+  );
+}
